31903417f8fe93ffc0f8cf3e74492bbedf37b878
sanghyun lee <58983068+ggapsang@users.noreply.github.com>
2024-04-05 07:29:31 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,10 +9,6 @@ class AddData():
     # 데이터 추가 함수 구현
     pass
 
-class ViewData():
-    # 데이터 조회 함수 구현
-    pass
-
 class UpdateData():
     # 데이터 업데이트 함수 구현
     pass
@@ -21,19 +17,22 @@ class DeleteData():
     # 데이터 삭제 함수 구현
     pass
 
-class OnOffFlag() :
+class SetOnOff() :
     # 공정별 데이터 조작 가능 여부 권한 조정
+    pass
+
+class ViewProcess():
     pass
 
 def load_menu():
     # 메뉴 파일이 위치한 경로1
-    menu_file_path = 'menu.txt'
-    if os.path.exists(menu_file_path):
-        with open(menu_file_path, 'r', encoding='utf-8') as file:
+    menu_f_path = 'menu.txt'
+    if os.path.exists(menu_f_path):
+        with open(menu_f_path, 'r', encoding='utf-8') as file:
             menu_content = file.read()
         return menu_content
     else:
-        return "메뉴 파일을 찾을 수 없습니다."
+        return f"e : {menu_f_path}"
 
 def main_menu():
     menu_content = load_menu()  # 메뉴 내용 불러오기
@@ -50,10 +49,10 @@ def main_menu():
         elif choice == '4':
             delete_data()
         elif choice == '5':
-            print("프로그램을 종료합니다.")
             break
         else:
-            print("잘못된 입력입니다. 다시 시도하세요.")
+            print("error")
+            print(menu_content)
 
 # 아래 함수들(add_data, view_data, update_data, delete_data)은 구현이 필요합니다.
 
